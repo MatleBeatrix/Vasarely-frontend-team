abecf042b49314b00a62ed2db38207d19af8ded5
Jonathon Nagy <nagy.jon93@gmail.com>
2022-02-17 01:07:50 +0100

    registration

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,39 @@
+const fs = require('fs')
+const cors = require('cors')
 const express = require('express')
 const app = express()
-const cors = require('cors');
-const port = 3000
+const port = 4000
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+
+const users = require('./data/users.json')
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/gallery', (req, res) => {
-  res.send('Hello World!')
+app.post('/api/signup', (req, res) => {
+  if (!req.body.username || !req.body.password) {
+	  return res.sendStatus(400);
+  } 
 
+  for(let user of users){
+	  if(user.username === req.body.username){
+		  return res.sendStatus(409)
+	  }
+	}
+
+	//todo
+	const newUser = {
+		username: req.body.username,
+		password: req.body.password
+	}
+	
+	users.push(newUser);
+	fs.writeFileSync('./data/users.json', JSON.stringify(users));
+	res.sendStatus(200);
 })
 
 app.listen(port, () => {
