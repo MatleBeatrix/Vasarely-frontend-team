b59531ed1791dfeafd108aeacd45b4c6f83c78cb
Jonathon Nagy <nagy.jon93@gmail.com>
2022-02-17 01:09:14 +0100

    refactor & mui pagination

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,38 @@
 // import './App.css';
 import http from 'axios';
-import PicturesCard from './PicturesCard';
-import { useState,useEffect } from "react";
-
+import { useState, useEffect } from "react";
+import { Pagination } from '@mui/material';
+import PicturesCard from './components/PicturesCard';
+import Registration from './components/Registration';
 
 
 function App() {
   let [pageNumber, setPageNumber] = useState(1);
+  let [totalPages, setTotalPages] = useState(0)
   let [pageData, setPageData] = useState([])
 
-  const recivePage = http.get(`https://api.harvardartmuseums.org/image?apikey=95ab7a44-f4f7-44df-97b4-fcaad30a3961&page=${pageNumber}`)
-  // .then(res => setPageData(res.data.records))
+  const load = async () => {
+    const res = await http.get(`https://api.harvardartmuseums.org/image?apikey=95ab7a44-f4f7-44df-97b4-fcaad30a3961&page=${pageNumber}`)
 
-
-
+    setPageData(res.data.records)
+    setTotalPages(res.data.info.pages)
+    console.log(res)
+  }
+  
   useEffect(() => {
-    const consoleRecievedRequest = (data) => {
-      console.log(data)
-      setPageData(data.data.records)
-    };
-    recivePage.then(consoleRecievedRequest)
-
+    load()
   }, [pageNumber])
-  
 
+  console.log(totalPages)
   console.log(pageData)
-  
+
   return (
     <div className="App">
-      <button onClick={(event) => {setPageNumber(pageNumber-1);}}>Back</button>
-      <button onClick={(event) => {setPageNumber(pageNumber+1);}}>Next</button>
-      <div>{pageData.map(pic => <PicturesCard pic={pic} />)}</div>
+      <Registration />
+      <Pagination count={totalPages} onChange={(event, value) => setPageNumber(value)}/>
+      {/* <button onClick={() => { setPageNumber(pageNumber - 1); }}>Back</button>
+      <button onClick={() => { setPageNumber(pageNumber + 1); }}>Next</button> */}
+      <div>{pageData.map(pic => <PicturesCard key={pic.id} pic={pic} />)}</div>
     </div>
   );
 }
