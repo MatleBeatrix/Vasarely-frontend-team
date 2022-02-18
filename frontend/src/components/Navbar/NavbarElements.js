ef1906861b7750ae8cd36d0ba99d589786b7c95a
ormate99 <ormate99@gmail.com>
2022-02-18 01:14:30 +0100

    Navbar, popup background color edit

--- frontend/src/components/Navbar/NavbarElements.js
+++ frontend/src/components/Navbar/NavbarElements.js
@@ -3,13 +3,14 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-    background: orangered;
-    height: 85px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0.2rem calc((100vw - 1000px) / 2);
-    z-index: 12;
+  background: rgb(132,145,172);
+  background: radial-gradient(circle, rgba(132,145,172,1) 40%, rgba(2,0,36,1) 100%);
+  height: 85px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.2rem calc((100vw - 1000px) / 2);
+  z-index: 12;
 `;
 export const NavLogo = styled(Link)`
   cursor: pointer;
