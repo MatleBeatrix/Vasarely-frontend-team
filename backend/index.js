27f2f6136335c5b0670f3e22e86954708ba7e2e8
Jonathon Nagy <nagy.jon93@gmail.com>
2022-02-22 15:45:40 +0100

    registration and login

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(express.json())
 
 const users = require('./data/users.json')
 
+const sessions = {}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -28,12 +29,33 @@ app.post('/api/signup', (req, res) => {
 	//todo
 	const newUser = {
 		username: req.body.username,
-		password: req.body.password
+		password: req.body.password,
+		todoList: []
 	}
 	
 	users.push(newUser);
 	fs.writeFileSync('./data/users.json', JSON.stringify(users));
 	res.sendStatus(200);
+})
+
+app.post('/api/login', (req,res) => {
+	const auth = req.header('Authorization')
+	if (!auth) return res.sendStatus(401)
+
+	const credentials = auth.split('&&&');
+	const username = credentials[0];
+	const password = credentials[1];
+	const user = users.find(user => username === user.username && password === user.password)
+	if (!user) return res.sendStatus(401)
+	let sessionId = Math.random().toString()
+	sessions[sessionId] = user
+	console.log(sessions)
+	// setTimeout(() => {
+	// 	delete sessions[sessionId]
+	// 	console.log('Session end')
+	// }, 6*10*1000);
+	res.json(sessionId)
+
 })
 
 app.listen(port, () => {
