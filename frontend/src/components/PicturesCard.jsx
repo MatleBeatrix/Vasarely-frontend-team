ccfd1648fc9f2bbd1dc7407b3deb77c716a7a9e4
Beatrix Mátlé <matlebeatrix@edu.bme.hu>
2022-02-22 17:03:28 +0100

    ok

--- frontend/src/components/PicturesCard.jsx
+++ frontend/src/components/PicturesCard.jsx
@@ -3,16 +3,21 @@ import { useState } from "react";
 import Popup from './Popup';
 import Popup2 from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import noPicture from '../components/nopic.jpg'
 
 
 const Colors = ({pic}) => {
   if (pic.colors != null){
     return (<div className='colors-box'>
       {pic.colors.map((c, index) => {
-                  return <Popup2 trigger={<div className="colors" style={{backgroundColor: c.color}} key={index}> </div>} position="right center">
-                  <div>{c.color}</div>
+                  return (
+                  <div key={index}>
+                    <Popup2 trigger={<div className="colors" style={{backgroundColor: c.color}}> </div>} position="right center">
+                      <div>{c.color}</div>
+                    </Popup2>
 
-                </Popup2>
+                  </div>
+                )
     })}
     </div>)
 
@@ -24,36 +29,108 @@ const Colors = ({pic}) => {
   }
 }
 
-const PicturesCard = ({ pic }) => {
 
+
+const PicturesCard = ({ pic }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const togglePopup = () => {
     setIsOpen(!isOpen);
   }
 
+  //console.log(pic)
+  //console.log(pic.people)
+  //console.log(pic.primaryimageurl)
+
   return (
     <div>
-      <input type="image" id="image" alt={pic.imageid}
-        src={pic.baseimageurl} onClick={togglePopup} />
-
+      <div className='galleryImageBox' onClick={togglePopup}>
+        {(pic.primaryimageurl !== null && pic.primaryimageurl !== undefined) &&
+          <input type="image" className="galleryImage" alt={pic.imageid} src={pic.primaryimageurl}  />
+        }
+        {(pic.primaryimageurl === null) &&
+        /*<input type="image" id="image" alt={pic.imageid} src={"https://harvardartmuseums.org/collections/object/356250"} onClick={togglePopup} />*/
+          <input type="image" className="galleryImage" alt={pic.imageid} src={noPicture} />
+        }
+        {(pic.primaryimageurl === undefined) &&
+          <input type="image" className="galleryImage" alt={pic.imageid} src={noPicture}  />
+        }
+      </div>
+      
+      <div className='popup'>
       {isOpen && <Popup
         content={<>
-          <img src={pic.baseimageurl} alt={pic.imageid} />
+
+        <div className='popupTop'>
+        {(pic.primaryimageurl !== null && pic.primaryimageurl !== undefined) &&
+            <img className="popupImage" src={pic.primaryimageurl} alt={pic.imageid} />
+          }
+          {(pic.primaryimageurl === null || pic.primaryimageurl === undefined) &&
+            <img className="popupImage" src={noPicture} alt={pic.imageid} />
+          }
           <div>
-            <p>{pic.date}</p>
-            <p>Description:</p>
-            {pic.description === null && <p>no data</p>}
-            {pic.description !== null && <p>{pic.description}</p>}
-            <p>Technique:</p>
-            {pic.technique === null && <p>no data</p>}
-            {pic.technique !== null && <p>{pic.technique}</p>}
-            <p>Colors:</p>
-            {<Colors pic={pic} />}
+            <h2>{pic.title}</h2>
+
+            {(pic.people !== null && pic.people !== undefined) &&
+              <div className="popupDataBox">
+                <h3>Artist:</h3>
+                <div>
+                  {pic.people.map((person, index) => <p key={index}>{person.name}</p>)}
+                </div>
+              </div>
+            }
+            {pic.century !== null &&
+              <div className="popupDataBox">
+                <h3>Century:</h3>
+                <p>{pic.century}</p>
+              </div>
+            }
+            {pic.culture !== null &&
+              <div className="popupDataBox">
+                <h3>Culture:</h3>
+                <p>{pic.culture}</p>
+              </div>
+            }
+            {pic.technique !== null &&
+              <div className="popupDataBox">
+                <h3>Technique:</h3>
+                <p>{pic.technique}</p>
+              </div>
+            }
+
+            {(pic.worktypes !== null && pic.worktypes !== undefined) &&
+              <div className="popupDataBox">
+                <h3>Worktypes:</h3>
+                <div>
+                  {pic.worktypes.map(type => <p key={type.worktypeid}>{type.worktype}</p>)}
+                </div>
+              </div>
+            }
+            {(pic.colors !== null && pic.colors !== undefined) &&
+            <div className="popupDataBox">
+              <h3>Colors:</h3>
+              {<Colors pic={pic} />}
+            </div>
+            }
+        </div>
+        <div>
+          
+        </div>
+          
+
           </div>
+                      
+          {pic.description !== null &&
+              <div className='des'>
+                <h3>Description:</h3>
+                <hr></hr>
+                <p>{pic.description}</p>
+              </div>
+            }
         </>}
         handleClose={togglePopup}
       />}
+      </div>
 
       {/* <img src={pic.baseimageurl} alt={pic.imageid}/> */}
       {/* <p>{pic.date}</p> */}
--- frontend/src/components/pages/gallery.js
+++ frontend/src/components/pages/gallery.js
@@ -4,41 +4,142 @@ import http from 'axios';
 import { useState, useEffect } from "react";
 import { Pagination } from '@mui/material';
 import PicturesCard from '../PicturesCard';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+
 
 const Gallery = () => {
-
   let [pageNumber, setPageNumber] = useState(1);
-  let [totalPages, setTotalPages] = useState(1)
-  let [pageData, setPageData] = useState([])
+  let [totalPages, setTotalPages] = useState(1);
+  let [pageData, setPageData] = useState([]);
+
+
+  let [cultures, setCultures] = useState([]);
+  let [worktypes, setWorktypes] = useState([]);
+  let [classifications, setClassifications] = useState([]);
+  let [periods, setPeriods] = useState([]);
+
+  let [searchCulture, setSearchCulture] = useState("");
+  let [searchCentury, setSearchCentury] = useState("");
+  let [searchWorktype, setSearchWorktype] = useState("");
+  let [searchPeriod, setSearchPeriod] = useState("");
+  let [searchTechnique, setSearchTechnique] = useState("");
+  let [searchClassification, setSearchClassification] = useState("");
+
+
+  const Dropdownlist = ({list, label, value}) => {
+    const handleChange = (event) => {
+      if (label==="Culture") setSearchCulture(event.target.value);
+      if (label==="Worktype") setSearchWorktype(event.target.value);
+      if (label==="Classification") setSearchClassification(event.target.value);
+      if (label==="Period") setSearchPeriod(event.target.value);
+    };
+
+    const divStyle = {
+      color: 'royalblue'
+      
+    };
+
+    return(
+      <Box sx={{ minWidth: 120, maxWidth: 300 }}>
+      <FormControl fullWidth>
+
+        <InputLabel style={divStyle} id="demo-simple-select-label">{label}</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={value}
+          label={label}
+          onChange={handleChange}
+        >
+            {list.map((data) => <MenuItem  key={data.id} value={data.id}>{data.name}</MenuItem>)}
+        </Select>
+      </FormControl>
+      </Box>
+    )
+  }
+
+  const loadSearch = async (category) => {
+    try {
+      const testres = await http.get(`https://api.harvardartmuseums.org/${category}?apikey=95ab7a44-f4f7-44df-97b4-fcaad30a3961`)
+      const res = await http.get(`https://api.harvardartmuseums.org/${category}?size=${testres.data.info.totalrecords}&sort=name&apikey=95ab7a44-f4f7-44df-97b4-fcaad30a3961`)
+      const newArray =[];
+      
+      newArray.push({"id": "", "name": "<<ALL>>"});
+      res.data.records.map(data => {
+        let newSearch = {
+          "id": data.id,
+          "name": data.name
+        }
+        newArray.push(newSearch);
+      });
+
+      if (category === "culture") setCultures(newArray);
+      if (category === "worktype") setWorktypes(newArray);
+      if (category === "classification") setClassifications(newArray);
+      if (category === "period") setPeriods(newArray);
+    }
+    catch(error) {
+      error.alert("No data")
+    }
+  }
 
   const load = async () => {
-    const res = await http.get(`https://api.harvardartmuseums.org/image?apikey=95ab7a44-f4f7-44df-97b4-fcaad30a3961&page=${pageNumber}`)
-
-    setPageData(res.data.records)
-    setTotalPages(res.data.info.pages)
-    console.log(res)
-    console.log(res.data.info.prev)
+    try {
+      const res = await http.get(`https://api.harvardartmuseums.org/object?size=24&sortorder=desc&sort=primaryimageurl&culture=${searchCulture}&century=${searchCentury}&worktype=${searchWorktype}&period=${searchPeriod}&classification=${searchClassification}&apikey=95ab7a44-f4f7-44df-97b4-fcaad30a3961&page=${pageNumber}`)
+      //console.log(res.data.info.pages)
+      //console.log(res);
+      //console.log(res.data);
+      setTotalPages(res.data.info.pages)
+      setPageData(res.data.records)
+    }
+    catch(error) {
+      error.alert("No data")
+    }
   }
 
   useEffect(() => {
-    load()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    load();
   }, [pageNumber])
 
-  // console.log(totalPages)
-  console.log(pageData)
-  console.log(pageNumber)
+  useEffect(() => {
+    loadSearch("culture");
+    loadSearch("worktype");
+    loadSearch("classification");
+    loadSearch("period");
+    
+  }, [])
+
 
   return (
-	<div>
-
-    <div className='pagination'>
-    <Pagination variant="outlined" size="large" count={totalPages} onChange={(event, value) => setPageNumber(value)} />
+    <div>
+      <div className='searchBox'>
+        <div className='listBox'><Dropdownlist list={cultures} label={"Culture"} value={searchCulture}/></div>
+        <div className='listBox'><Dropdownlist list={worktypes} label={"Worktype"} value={searchWorktype}/></div>
+        <div className='listBox'><Dropdownlist list={classifications} label={"Classification"} value={searchClassification}/></div>
+        <div className='listBox'><Dropdownlist list={periods} label={"Period"} value={searchPeriod}/></div>
+        
+        <button onClick={(e) => {setPageNumber(1); load()}} >Search</button>
+        {/*
+          <button onClick={(e) => {setSearchCulture(''); setSearchWorktype(''); setSearchClassification(''); setSearchPeriod('')}} >Clear</button>
+        */
+        }
+      </div>
+      <div>
+        <div className='pagination'>
+        <Pagination variant="outlined" size="large" count={totalPages} boundaryCount={1} siblingCount={3} onChange={(event, value) => setPageNumber(value)} />
+        </div>
+        <div id="pics">{pageData.map(pic => <PicturesCard key={pic.id} pic={pic} />)}</div>
+        <div className='pagination'>
+        <Pagination variant="outlined" size="large" count={totalPages} boundaryCount={1} siblingCount={3} onChange={(event, value) => setPageNumber(value)} />
+        </div>
+      </div>
     </div>
-    <div id="pics">{pageData.map(pic => <PicturesCard key={pic.id} pic={pic} />)}</div>
-
-  </div>
   )
+
 }
 
 export default Gallery
